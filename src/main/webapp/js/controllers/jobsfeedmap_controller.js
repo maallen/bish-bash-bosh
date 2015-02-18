a89b81e6afa21959f0f2c502bb05dae55b449322
Mark Allen <maallen87@gmail.com>
2015-02-18 23:47:17 +0000

    Changed from single map markers to markers html tag

--- src/main/webapp/js/controllers/jobsfeedmap_controller.js
+++ src/main/webapp/js/controllers/jobsfeedmap_controller.js
@@ -1,10 +1,39 @@
 function JobsFeedMapController($scope, $http){
-	$scope.marker = {
-		id: 0,
-		coords: {
-			latitude: 53.4254167,
-			longitude: -7.9243681
+	$scope.map = {
+		center: {
+			latitude: 53.428665,
+			longitude: -8.3320801
 		},
-		options: { draggable: false}
+		zoom: 8,
+		bounds: {}
 	};
+
+	$scope.markers =[];
+
+	var marker1 = {
+		id: 0,
+		latitude: 53.428665,
+		longitude: -8.3320801,
+		title: 1
+	};
+
+	var marker2 = {
+		id: 1,
+		latitude: 53.3965882,
+		longitude: -7.9021542,
+		title: 2
+	};
+
+	$scope.markers.push(marker1);
+	$scope.markers.push(marker2);
+
+
+	// $scope.marker = {
+	// 	id: 0,
+	// 	coords: {
+	// 		latitude: 53.4254167,
+	// 		longitude: -7.9243681
+	// 	},
+	// 	options: { draggable: false}
+	// };
 }
