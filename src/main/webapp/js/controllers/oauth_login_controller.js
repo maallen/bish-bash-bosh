f2f6703229cbd7f025e6a5e7c3cedfd331698511
Robert <robfitz85@gmail.com>
2015-03-04 00:08:34 +0000

    Trivial - Minor change to redirect_uri so that it will work in openshift

--- src/main/webapp/js/controllers/oauth_login_controller.js
+++ src/main/webapp/js/controllers/oauth_login_controller.js
@@ -8,7 +8,9 @@ myAppModule.controller('LoginControllerOauth',function($scope) {
     	console.log("Clicked Login");
     	var client_id="946568828798-ob0dnlcdhe30kbgajfdp26it3mt4pb86.apps.googleusercontent.com";
     	var scope="email";
-    	var redirect_uri="http://localhost:8080/caash/";
+    	// Comment out the below when working locally
+//    	var redirect_uri="http://localhost:8080/caash/";
+    	var redirect_uri="http://caash-caash.rhcloud.com/";
     	var response_type="token";
     	var url="https://accounts.google.com/o/oauth2/auth?scope="+scope+"&client_id="+client_id+"&redirect_uri="+redirect_uri+
     	"&response_type="+response_type;
